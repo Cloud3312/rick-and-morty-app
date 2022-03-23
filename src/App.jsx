d503ccd85993df9d7f07d0d8dba355f6e0b74267
Braian Espinoza <braian3000@gmail.com>
2022-03-22 23:27:28 -0300

    Se inicializa el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand text-uppercase" href="/">
+            Rick and morty
+          </a>
+        </div>
+      </nav>
+
+      <div>hola</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,6 @@
+const Navbar = () => {
+  // const [notes, setNotes] = useState([]);
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
