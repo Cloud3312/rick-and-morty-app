e98f10593fc152a653c3fd5352f1077941b02590
Braian Espinoza <braian3000@gmail.com>
2022-03-22 23:39:45 -0300

    se inicializa el proyecto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,11 @@
 const Navbar = () => {
   // const [notes, setNotes] = useState([]);
-  return <div>Navbar</div>;
+  return (
+    <>
+      <div>Navba asdasr</div>
+      <h1>asda</h1>
+    </>
+  );
 };
 
 export default Navbar;
