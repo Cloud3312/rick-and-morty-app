30688b1b55deb562d71c0d3fdbba3b2120963ee7
Braian Espinoza <braian3000@gmail.com>
2022-04-01 21:49:45 -0300

    Se agregan la informacion de cada card

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,25 @@ import Filters from "./components/Filter/Filters";
 const App = () => {
   let [pageNumber, setPageNumber] = useState(1);
   let [fetchedData, setFetchedData] = useState([]);
-  let { info, results } = fetchedData; //data de la api
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}",`;
-
+  let { info, results } = fetchedData; //data de la api desestructurada
+  let api = `https://rickandmortyapi.com/api/character/?page=1`;
+  console.log(results);
   //obtengo toda la data de la api
+  // useEffect(() => {
+  //   const getDataFromAPI = async () => {
+  //     const data = await fetch(api).then((res) => res.json());
+  //     setFetchedData(data);
+  //   };
+  //   getDataFromAPI();
+  // }, [api]);
+
   useEffect(() => {
-    let getDataFromAPI = async () => {
-      let data = await fetch(api).then((res) => {
-        res.json();
-        setFetchedData(data);
-      });
-    };
+    (async function () {
+      const data = await fetch(api).then((res) => res.json());
+      setFetchedData(data);
+    })();
   }, [api]);
+
   return (
     <>
       <div className="App">
@@ -34,9 +41,7 @@ const App = () => {
             </div>
             <div className="col-8">
               <div className="row">
-                <Cards />
-                <Cards />
-                <Cards />
+                <Cards results={results} />
               </div>
             </div>
           </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,32 @@
 import React from "react";
-
-const Cards = () => {
-  return <div className="col-4">Cards</div>;
+import "./Cards.css";
+const Cards = ({ results }) => {
+  let display;
+  if (results) {
+    display = results.map((result) => {
+      let { id, name, image, location, status } = result;
+      return (
+        <div key={id} className="col-4 mb-4 position-relative">
+          <div className="cardBorder">
+            <img src={image} className={`${`imagem`} img-fluid`} alt="" />
+            <div className="content">
+              <div className="fs-4 fw-bold mb-4">{name}</div>
+              <div>
+                <div className="fs-6 ">Last location </div>
+                <div className="fs-5">{location.name}</div>
+              </div>
+            </div>
+          </div>
+          <div className={`${`badge`} position-absolute badge ${status}`}>
+            {status}
+          </div>
+        </div>
+      );
+    });
+  } else {
+    display = "No characters Found";
+  }
+  return <>{display}</>;
 };
 
 export default Cards;
